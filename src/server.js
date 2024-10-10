84537d468a9bf09580277ae6f14ee3d2afdd467a
unknown <steen_hansen@yahoo.com>
2024-10-10 13:57:25 -0700

    for netlify

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+const serverless = require('serverless-http');
+
 var express = require('express');
 var { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
 var bodyParser = require('body-parser');
@@ -129,7 +131,26 @@ server.get('*', function (req, res) {
     res.redirect('/media-radio-lists');
 });
 
-server.listen(listen_port, () => {
-    console.log(`GraphQL Server is now running on ${http_host}/graphql`);
-    console.log(`View GraphiQL at ${http_host}/graphiql`);
-});
+//////////////////////////////////////////////////////////////
+// for Render.com
+//////////////////////////////////////////////////////////////
+/*  
+https://sffaudio-graph-ql.onrender.com/graphiql?operationName=serch_ql&query=query%20serch_ql(%24search_parameter%3A%20String!)%20%7B%0A%20%20search_site_content(search_text%3A%20%24search_parameter)%20%7B%0A%20%20%20%20%20%20...%20on%20ArticlePage%7B%20ID%20headline%20article_post%20%20%20%7D%2C%0A%20%20%20%20...%20on%20MentionPage%7B%20ID%20headline%20mention_post%20%20%20%7D%2C%0A%20%20%20%20...%20on%20RsdMedia%20%7B%20ID%20rsd_post%20resource%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20book%7B%20author%20title%20%7D%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20podcast%20%7B%20description%20mp3%20length%20episode%20%7D%20%20%20%7D%2C%0A%20%20%20%20...%20on%20SffAudioMedia%20%7B%20ID%20sffaudio_post%20narrator%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20possiblebook%7B%20author%20title%20%7D%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20podcast%20%7B%20description%20mp3%20length%20episode%20%7D%20%20%20%7D%0A%20%20%7D%0A%7D%0A&variables=%7B%0A%20%20%22search_parameter%22%3A%20%22Clarke%22%0A%7D
+*/
+
+// server.listen(listen_port, () => {
+//     console.log(`GraphQL Server is now running on ${http_host}/graphql`);
+//     console.log(`View GraphiQL at ${http_host}/graphiql`);
+// });
+
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////
+// for netlify
+// https://github.com/neverendingqs/netlify-express/blob/0780127cd575704e2a2a00a1a648ba5a5a66c388/express/server.js
+//////////////////////////////////////////////////////////////
+
+module.exports = server;
+module.exports.handler = serverless(server);
